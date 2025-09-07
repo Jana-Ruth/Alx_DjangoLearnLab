4b9c6b7b42a40fcacf2737635b34c71c2fa16a1a
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-07 21:42:02 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
 urlpatterns = [
       path("", views.list_books, name="list_books"),
     path("book/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
-    path('books/add/', views.add_book, name="add_book"),
-    path('books/<int:book_id>/edit/', views.edit_book, name="edit_book"),
-    path('books/<int:book_id>/delete/', views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
