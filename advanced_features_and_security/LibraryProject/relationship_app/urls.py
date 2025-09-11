70279ef68a24062c30e23029f7f5aae6fbf4fd8e
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-11 19:11:43 +0100

    update new project

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+from .views import list_books
+
+urlpatterns = [
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication URLs
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path('', include('relationship_app.urls')),
+]
+
+
+urlpatterns = [
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+]
+
+urlpatterns = [
+      path("", views.list_books, name="list_books"),
+    path("book/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
+]
