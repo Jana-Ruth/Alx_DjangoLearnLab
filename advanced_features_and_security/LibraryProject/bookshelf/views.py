b60fd8becc079ff2a8e3eafa99fd552c0dde9cc9
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-11 20:37:37 +0100

    form

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
-from .forms import BookForm, SearchForm, ExampleForm
+from .forms import BookForm, SearchForm
+from .forms import ExampleForm
 from django.views.decorators.http import require_http_methods
 
 
