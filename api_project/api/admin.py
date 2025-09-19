d84ddbc7c0e0cde0cc87d49f54366d9a9d47576d
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-19 13:52:23 +0100

    models

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
+[
+    {
+        "id": 1,
+        "title": "Django for Beginners",
+        "author": "William S. Vincent",
+        "published_date": "2023-01-10",
+        "isbn": "1234567890123"
+    }
+]
+s
