f96b00c376fd4b971649932b7f577901418ead67
Jana-Ruth <ruthewenode73@gmail.com>
2025-10-03 14:43:13 +0100

    update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'taggit',
 ]
 
 # Templates - ensure DIRS includes the top-level templates folder
