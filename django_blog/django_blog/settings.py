6fc602b7cd01667b48342e2be81d228d7019e4fa
Jana-Ruth <ruthewenode73@gmail.com>
2025-10-03 06:58:01 +0100

    update setting

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -103,6 +103,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',   # or your database host
+        'PORT': '5432',        # default PostgreSQL port
     }
 }
 
